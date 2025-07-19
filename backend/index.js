f9896e87062c39b9825ff7af8aa5cd39e0a9fbd6
Yash Dhumal <yashdhumal8484@gmail.com>
2025-07-20 00:01:46 +0530

    trigger render deploy

--- backend/index.js
+++ backend/index.js
@@ -15,16 +15,16 @@ const allowedOrigins = [
     "http://localhost:5173",
     "https://job-hunt-theta-nine.vercel.app"
 ];
-const corsOptions = {
-    origin: function (origin, callback) {
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error("Not allowed by CORS"));
-        }
-    },
-    credentials: true
-};
+// const corsOptions = {
+//     origin: function (origin, callback) {
+//         if (!origin || allowedOrigins.includes(origin)) {
+//             callback(null, true);
+//         } else {
+//             callback(new Error("Not allowed by CORS"));
+//         }
+//     },
+//     credentials: true
+// };
 // app.use(cors(corsOptions));
 app.use(cors({
     origin: "*",
