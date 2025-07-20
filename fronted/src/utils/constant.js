936303cfe3db304afc6baaef92e37149d17e77ec
Yash Dhumal <yashdhumal8484@gmail.com>
2025-07-20 18:04:50 +0530

    Switched to VITE_API_URL for environment-based API config

--- fronted/src/utils/constant.js
+++ fronted/src/utils/constant.js
@@ -1,8 +1,10 @@
-export const USER_API_END_POINT="http://localhost:8000/api/v1/user"
-export const JOB_API_END_POINT="http://localhost:8000/api/v1/job"
-export const APPLICATION_API_END_POINT="http://localhost:8000/api/v1/application"
-export const COMPANY_API_END_POINT="http://localhost:8000/api/v1/company"
-// export const USER_API_END_POINT="https://jobhunt-1-fyzh.onrender.com/api/v1/user"
-// export const JOB_API_END_POINT="https://jobhunt-1-fyzh.onrender.com/api/v1/job"
-// export const APPLICATION_API_END_POINT="https://jobhunt-1-fyzh.onrender.com/api/v1/application"
-// export const COMPANY_API_END_POINT="https://jobhunt-1-fyzh.onrender.com/api/v1/company"
+// export const USER_API_END_POINT="http://localhost:8000/api/v1/user"
+// export const JOB_API_END_POINT="http://localhost:8000/api/v1/job"
+// export const APPLICATION_API_END_POINT="http://localhost:8000/api/v1/application"
+// export const COMPANY_API_END_POINT="http://localhost:8000/api/v1/company"
+const BASE_URL = import.meta.env.VITE_API_URL;
+
+export const USER_API_END_POINT = `${BASE_URL}/api/v1/user`;
+export const JOB_API_END_POINT = `${BASE_URL}/api/v1/job`;
+export const APPLICATION_API_END_POINT = `${BASE_URL}/api/v1/application`;
+export const COMPANY_API_END_POINT = `${BASE_URL}/api/v1/company`;
