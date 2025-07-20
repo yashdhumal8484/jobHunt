e04c37348a2cd37a4db8c23ad569e920081e95da
YASH DEEPAK DHUMAL <165413991+yashdhumal8484@users.noreply.github.com>
2025-07-20 18:57:10 +0530

    Update constant.js

--- fronted/src/utils/constant.js
+++ fronted/src/utils/constant.js
@@ -2,9 +2,9 @@
 // export const JOB_API_END_POINT="http://localhost:8000/api/v1/job"
 // export const APPLICATION_API_END_POINT="http://localhost:8000/api/v1/application"
 // export const COMPANY_API_END_POINT="http://localhost:8000/api/v1/company"
-const BASE_URL = import.meta.env.VITE_API_URL;
+// const BASE_URL = import.meta.env.VITE_API_URL;
+export const USER_API_END_POINT = `https://jobhunt-backend-a9od.onrender.com/api/v1/user`;
+export const JOB_API_END_POINT = `https://jobhunt-backend-a9od.onrender.com/api/v1/job`;
+export const APPLICATION_API_END_POINT = `https://jobhunt-backend-a9od.onrender.com/api/v1/application`;
+export const COMPANY_API_END_POINT = `https://jobhunt-backend-a9od.onrender.com/api/v1/company`;
 
-export const USER_API_END_POINT = `${BASE_URL}/api/v1/user`;
-export const JOB_API_END_POINT = `${BASE_URL}/api/v1/job`;
-export const APPLICATION_API_END_POINT = `${BASE_URL}/api/v1/application`;
-export const COMPANY_API_END_POINT = `${BASE_URL}/api/v1/company`;
